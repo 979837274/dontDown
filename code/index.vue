8c4bf7f6e14eef0757c34f3fecb50cf7e09b6b99
憨狗 <33802670+979837274@users.noreply.github.com>
2021-05-05 13:00:58 +0800

    Add files via upload

---
+++ code/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <calender></calender>
+  </div>
+</template>
+
+<script>
+import Calender from './component/main.vue'
+
+export default {
+  components: {
+    Calender
+  }
+}
+</script>
+
+<style>
+
+</style>
